797995f83810a361077c81323694e2b0cf2d1a3f
Tony Leng <lengtk@gmail.com>
2023-02-20 11:35:49 +0900

    linked and tested all components to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Intro from './components/Intro';
+import StackIcons from './components/StackIcons';
+import AboutMe from './components/AboutMe';
+import Playerwon from './components/Playerwon';
+import Healthway from './components/Healthway';
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar />
+      <Intro />
+      <StackIcons />
+      <AboutMe />
+      <Playerwon />
+      <Healthway />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,9 @@
 import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>TEST</h1>
+    </nav>
+  )
+}
