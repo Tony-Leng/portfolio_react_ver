940b79043207cd09ea015e26dfc84d4fbdc6c4a0
Tony Leng <lengtk@gmail.com>
2023-02-21 21:50:31 +0900

    added all images to images folder

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default function Intro() {
   return (
-    <h1>TEST</h1>
+    <div class="initial-view d-flex">
+    <div class="name-title">
+      <h1 class="name">Tony Leng</h1>
+      <h3 class="title">Front-End Web Developer</h3>
+      <div class="socialicons">
+        <a href="https://github.com/Tony-Leng" target="_blank"><i class="fa-brands fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/tony-leng/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+        <a href="mailto: lengtk@gmail.com"><i class="fa-solid fa-square-envelope"></i></a>
+      </div>
+    </div>
+      <div>
+      <img id="home-img" src="images/ProfilePic.png" alt=""></img>
+    </div>
+    <div class="arrow bounce d-lg-none">
+      <div class="fa fa-arrow-down fa-2x" href=""></div>
+    </div>
+  </div>
   )
 }
