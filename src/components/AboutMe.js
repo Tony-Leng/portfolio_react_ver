3861f0092de7e60a80797ddb636ec2bf265c1542
Tony Leng <lengtk@gmail.com>
2023-02-22 09:33:39 +0900

    added content to AboutMe

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Intro() {
   return (
-    <h1>This is AboutMe</h1>
-  )
+    <div>
+      <h2 id="aboutme">About Me</h2>
+      <p class="aboutmetext">Thanks for visiting my portfolio. My name is Tony and I was born and raised in San Diego, California, USA to refugee parents from Cambodia. I've been living and working in Tokyo, Japan since 2017. I enjoy front-end developing and UI design for the web. </p>
+    </div>
+    )
 }
