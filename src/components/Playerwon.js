05b276aec3b70bd9142f6e5b3817a3b35898549a
Tony Leng <lengtk@gmail.com>
2023-02-22 09:37:33 +0900

    added content to Playerwon

--- src/components/Playerwon.js
+++ src/components/Playerwon.js
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function Intro() {
   return (
-    <h1>This is PlayerWON</h1>
+    <div>
+      <h2 id="myprojects">My Projects</h2>
+      <div class="container">
+        <div class="card-projects d-flex">
+          <div class="card-description">
+              <h4>PlayerWON</h4>
+              <p>Inspired by AirBnB, PlayerWON is a marketplace app that allows gamers to connect and find coaching or provide coaching. (Desktop only)</p>
+              <p>My role for this project was <strong>Front End Lead</strong>.</p>
+              <p>Html | CSS | JS | Stimulus | Ruby on Rails | PostgreSQL</p>
+          </div>
+          <div class="projectimg"  id="playerwon">
+            <img class="projectimg" src="images/playerwon.png" alt=""></img>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
