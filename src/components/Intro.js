d81bdc53e4e2324356842c556da22db1a95a56f7
Tony Leng <lengtk@gmail.com>
2023-02-26 21:13:48 +0900

    reformatted class to className in Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function Intro() {
   return (
-    <div class="initial-view d-flex">
-    <div class="name-title">
-      <h1 class="name">Tony Leng</h1>
-      <h3 class="title">Front-End Web Developer</h3>
-      <div class="socialicons">
+    <div className="initial-view d-flex">
+    <div className="name-title">
+      <h1 className="name">Tony Leng</h1>
+      <h3 className="title">Front-End Web Developer</h3>
+      <div className="socialicons">
         <a href="https://github.com/Tony-Leng" target="_blank"><i class="fa-brands fa-github"></i></a>
         <a href="https://www.linkedin.com/in/tony-leng/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
         <a href="mailto: lengtk@gmail.com"><i class="fa-solid fa-square-envelope"></i></a>
@@ -15,8 +15,8 @@ export default function Intro() {
       <div>
       <img id="home-img" src="images/ProfilePic.png" alt=""></img>
     </div>
-    <div class="arrow bounce d-lg-none">
-      <div class="fa fa-arrow-down fa-2x" href=""></div>
+    <div className="arrow bounce d-lg-none">
+      <div className="fa fa-arrow-down fa-2x" href=""></div>
     </div>
   </div>
   )
