c6f84bb84341562a480812f77eff500090ee9049
Tony Leng <lengtk@gmail.com>
2023-02-27 23:58:01 +0900

    styled font in Intro section

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,7 @@ export default function Intro() {
     <div className="initial-view d-flex">
     <div className="name-title">
       <h1 class="name">Tony Leng</h1>
-      <h3 className="title">Front-End Web Developer</h3>
+      <h3 class="title">Front-End Web Developer</h3>
       <div className="socialicons">
         <a href="https://github.com/Tony-Leng" target="_blank"><i class="fa-brands fa-github"></i></a>
         <a href="https://www.linkedin.com/in/tony-leng/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
