0e00196d48f60351ea127074e349142a50778c1b
Tony Leng <lengtk@gmail.com>
2023-04-08 12:34:18 +0900

    started redesign of portfolio site, added Navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import Intro from './components/Intro';
-import StackIcons from './components/StackIcons';
-import AboutMe from './components/AboutMe';
-import Playerwon from './components/Playerwon';
-import Healthway from './components/Healthway';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import useWindowSize from './components/useWindowSize';
 
 function App() {
-
-  const size = useWindowSize();
-
   return (
     <div className='App'>
-      {size.width > 540 && <Navbar />}
-      <Intro />
-      <StackIcons />
-      <AboutMe />
-      <Playerwon />
-      <Healthway />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import {Navbar, Container} from "react-bootstrap";
 
 export default function Navbar() {
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    // onScroll is to detect when user scrolls down to change background color of Navbar
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener('scroll', onScroll);
+
+    // remove when the component is removed from the DOM
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [])
+
   return (
-    <nav className='navbar'>
-      <h1>This is the Navbar</h1>
-    </nav>
+    <Navbar expand='lg'>
+      <Container>
+        <Navbar.brand href='#home'>
+          <img src={''} alt='logo'/>
+        </Navbar.brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className='navbar-toggler-icon'></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#skills'>Skills</Nav.Link>
+            <Nav.Link href='#projects'>Projects</Nav.Link>
+          </Nav>
+          <span className='navbar-text'>
+            <div className='social-icon'>
+              <a href='#'><img src={''} alt=''/></a>
+              <a href='#'><img src={''} alt=''/></a>
+              <a href='#'><img src={''} alt=''/></a>
+            </div>
+            <button className='contactme' onClick={() => console.log('contact me')}>
+              <span>Contact Me</span>
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
