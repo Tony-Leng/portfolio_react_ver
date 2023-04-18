051d25552e045c8b60974cd07db86d75b336d452
Tony Leng <lengtk@gmail.com>
2023-04-18 20:35:38 +0900

    added descriptions for Projects, updated HealthWay image

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,10 +4,11 @@ export default function ProjectCard({title, description, imgUrl, projectUrl}) {
   return (
     <Col sm={6} md={4}>
       <div className='proj-imgbx' >
-        <img src={imgUrl} alt='' onClick={projectUrl}/>
+        <img src={imgUrl} alt=''/>
         <div className='proj-txtx'>
           <h4>{title}</h4>
           <span>{description}</span>
+          {/* <button className='projectBtn' onClick={projectUrl}>View Project</button> */}
         </div>
       </div>
     </Col>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import { Container, Row, Tab, Nav, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCard";
 import playerwon from "../components/images/playerwon.png";
-import healthway from "../components/images/healthway.png";
+import healthway3 from "../components/images/healthway3.png";
 import ecomm from "../components/images/ecommerce_product_page.PNG";
 import creditcard from "../components/images/interactive_credit_card.PNG";
 import ratingcard from "../components/images/interactive_rating_card.PNG";
@@ -11,34 +11,34 @@ export default function Projects() {
   const projects = [
     {
       title: 'PlayerWON',
-      description: 'Description goes here',
+      description: 'PlayerWON is a marketplace app made for gamers looking for game coaches. My main roles for this project were UI design and frontend development.',
       imgUrl: playerwon,
     },
     {
       title: 'HealthWay',
-      description: 'Description goes here',
-      imgUrl: healthway,
+      description: 'HealthWay is an app made for tracking healtheck progress and paperwork. My main roles for this project were UI design and frontend development.',
+      imgUrl: healthway3,
     },
     {
       title: 'eCommerce Product Page',
-      description: 'Description goes here',
+      description: 'This is a frontend project found on Frontend Mentor whose aim is to replicate the design as closely as possible. I used React as the JS framework and this was my first experience with Tailwind.',
       imgUrl: ecomm,
       projectUrl: 'https://react-ecommerce-product-page.vercel.app/',
     },
     {
       title: 'Weather App',
-      description: 'Description goes here',
+      description: 'Made with HTML, CSS, and JS, this app pulls info from the OpenWeather API. My main focus here was practicing how to call APIs with JS.',
       // imgUrl: weatherapp,
     },
     {
       title: 'Interactive Credit Card Details',
-      description: 'Description goes here',
+      description: 'Another project from Frontend Mentor, this project is made with HTML, CSS, and JS. My main focus was practicing DOM manipulation and form validation.',
       imgUrl: creditcard,
       projectUrl: 'https://js-interactive-card-details-form.vercel.app/',
     },
     {
       title: 'Ratings Card',
-      description: 'Description goes here',
+      description: 'A simple project also from Frontend Mentor, it is made with HTML, CSS, and JS. My main focus was practicing handling user interactions and updating the DOM.',
       imgUrl: ratingcard,
       projectUrl: 'https://js-interactive-rating-component-main.vercel.app/',
     }
