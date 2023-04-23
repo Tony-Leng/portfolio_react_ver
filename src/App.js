90800383fe6180b0479391ef611d691db4f9a65d
Tony Leng <lengtk@gmail.com>
2023-04-23 17:29:15 +0900

    in App.js on master changed NavBar to Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from './components/NavBar';
+import NavBar from './components/Navbar';
 import Banner from './components/Banner';
 import Skill from './components/Skill';
 import Projects from './components/Projects';
