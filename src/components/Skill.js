78c37e07a6a5fcc3603181b8ce566970fb52199a
Tony Leng <lengtk@gmail.com>
2023-09-12 22:25:06 +0900

    added header to mobile Skills section

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -40,7 +40,7 @@ export default function Skills() {
       <Container>
         <Row>
           <div className="">
-            <h2 className="d-none d-sm-block d-md-none" >My Stack</h2>
+            <h2 className="d-block d-sm-none">My Stack</h2>
             <Col className="skill-box">
               <h2 className="mystack-d">My Stack</h2>
               <div className="skills-top flex mb-5">
