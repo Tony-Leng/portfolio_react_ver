86937c74332f6b2198c5f4e47a7fb37f30d96d4d
Tony Leng <lengtk@gmail.com>
2024-05-21 21:59:04 +0900

    updated tagline

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ import profilePic from "../components/images/ProfilePic.png"
 export default function Banner() {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const toRotate = ["Web Developer", "Web Designer"];
+  const toRotate = ["I'm an aspiring Product Manager"];
   const [text, setText] = useState('');
   const [index, setIndex] = useState(1);
   const [delta, setDelta] = useState(300 - Math.random() * 100);
@@ -52,7 +52,7 @@ const tick = () => {
         <Row className="align-items-center">
           <Col className="intro-text" xs={12} md={6} xl={7}>
             <span className="tagline">Hi, I'm Tony.</span>
-            <h1>{`I'm a `}<span className="txt-rotate" dataPeriod="1000" data-rotate='["Frontend Web Developer", "UI/UX Designer"]'><span className="wrap">{text}</span></span></h1>
+            <h1><span className="txt-rotate" dataPeriod="1000" data-rotate='["Frontend Web Developer", "UI/UX Designer"]'><span className="wrap">{text}</span></span></h1>
             <p>I was born and raised in San Diego, California, USA to refugee parents from Cambodia. I've been living and working in Tokyo, Japan since 2017. I enjoy front-end development and UI/UX design for the web.
             </p>
             <button className="hidden-sm" onClick={()=> window.location = 'mailto:lengtk@gmail.com'}>Contact Me <ArrowRightCircle size={25}/></button>
